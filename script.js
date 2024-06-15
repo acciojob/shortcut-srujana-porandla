473460a1b3b2cc48722fa8bb1695d2c2d7f4b320
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-15 13:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	if(str1.isEmpty || str2.isEmpty){
+		return "";
+	}else{
+		return str1.charAt(0)+str2charAt(0);
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
+//alert(shortcut(s1, s2));
